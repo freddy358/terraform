88d378ba4f62ff568f1583c101122379d68d3bd3
Farid C. Bakhishli <farid.baxishli@kapitalbank.az>
2020-11-19 00:06:12 +0400

    initial commit

--- 1_astahov/osnovi_terraform/lesson1.tf
+++ 1_astahov/osnovi_terraform/lesson1.tf
@@ -1,4 +1,24 @@
-resource "aws_instance" "example" {
-    ami            = "ami-2757f631"
-    instance_type  = "t2.micro"
+provider "aws" {}
+
+
+resource "aws_instance" "my_Ubuntu" {
+  ami           = "ami-090f10efc254eaf55"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name    = "My Ubuntu Server"
+    Owner   = "Farid Bakhishli"
+    Project = "Terraform Lessons"
+  }
+}
+
+resource "aws_instance" "my_Amazon" {
+  ami           = "ami-03a71cec707bfc3d7"
+  instance_type = "t3.small"
+
+  tags = {
+    Name    = "My Amazon Server"
+    Owner   = "Farid Bakhishli"
+    Project = "Terraform Lessons"
+  }
 }
